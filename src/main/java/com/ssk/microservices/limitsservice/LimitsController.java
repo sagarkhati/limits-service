27e007cb60a6bcb6c414c482c911f317e145e1f9
Sagar Singh Khati <sonu06sagar@gmail.com>
2023-04-09 22:58:48 +0530

    Adding Limits entity, Limits Controller and Some properties

---
+++ src/main/java/com/ssk/microservices/limitsservice/LimitsController.java
@@ -0,0 +1,13 @@
+package com.ssk.microservices.limitsservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsController {
+
+	@GetMapping("/limits")
+	public Limits retrieveLimits() {
+		return new Limits(1, 1000);
+	}
+}
